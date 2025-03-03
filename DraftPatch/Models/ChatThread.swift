8d80d3031795170a188c09edcae0b334855e0080
Robert DeLuca <robertdeluca19@gmail.com>
2025-03-03 17:52:37 -0600

    ✨ Allow renaming of conversations

--- DraftPatch/Models/ChatThread.swift
+++ DraftPatch/Models/ChatThread.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class ChatThread {
+class ChatThread: ObservableObject {
   @Attribute(.unique) var id: UUID
   var title: String
   var createdAt: Date
